2b0ac677d335c015bf38887f7418e7a70087f477
taioofh <123496181+taioofh@users.noreply.github.com>
2023-01-24 21:09:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(src){
+    img.src = src ;
+}
+function colors(color){
+    container.style.background = color ;
+
+}
